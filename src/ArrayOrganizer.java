c484dfa937faeb786c622208999ddd7924cc91ab
borabirlikci <borabirlikci@gmail.com>
2020-04-07 11:08:14 +0300

    master: initial commit

---
+++ src/ArrayOrganizer.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.Collections;
+//CS319
+public class ArrayOrganizer {
+    ArrayList<Integer> concatAndSort(ArrayList<Integer> list1, ArrayList<Integer> list2){
+        ArrayList<Integer> result = new ArrayList<>();
+        result.addAll(list1);
+        result.addAll(list2);
+        Collections.sort(result);
+        return result;
+    }
+}
