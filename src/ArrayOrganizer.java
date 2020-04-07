48fa96c1c2a4c61e2fa883b30c61698760d970fb
borabirlikci <borabirlikci@gmail.com>
2020-04-07 11:31:18 +0300

    featrue-branch: list3 added

--- src/ArrayOrganizer.java
+++ src/ArrayOrganizer.java
@@ -2,9 +2,10 @@ import java.util.ArrayList;
 import java.util.Collections;
 //CS319
 public class ArrayOrganizer {
-    ArrayList<Integer> concatAndSort(ArrayList<Integer> list1, ArrayList<Integer> list2){
+    ArrayList<Integer> concatAndSort(ArrayList<Integer> list1, ArrayList<Integer> list2, ArrayList<Integer> list3){
         ArrayList<Integer> result = new ArrayList<>();
         result.addAll(list1);
+        result.addAll(list2);
         result.addAll(list2);
         Collections.sort(result);
         return result;
